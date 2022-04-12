4331b51d36e2b7a5ffe1fb6aa44c7248cc7bfc13
raul <zarater.raul@gmail.com>
2022-04-12 08:47:03 -0700

    imageUrl to image_url as used in pics

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,38 +1,15 @@
 //1. start by creating a class component. Always start by importing React.
 import React from 'react';
-import './HornedBeast.css';
-import{Card} from 'react-bootstrap';
-import{Button} from 'react-bootstrap';
 //2. Declare the Class Component.
 class HornedBeast extends React.Component
 {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            favorites: 0,
-        }
-    }
-    myFav = () => {
-        this.setState({
-        favorites : this.state.favorites + 1
-        });
-    }
     render()
     {
         return(
             <>
-            <Card.Img class="image" variant = "top" alt={this.props.title} src={this.props.image_url}/>
-            <Card.Body>
-                <Card.Title>{this.props.title}</Card.Title>
-                <Card.Text> {this.state.favorites}</Card.Text>
-                <Card.Text>{this.props.description}</Card.Text>
-                <Button
-                variant="primary"
-                className='glow-on-hover'
-                onClick={this.myFav}>
-                </Button>
-            </Card.Body>
+            <h2>{this.props.title}</h2>
+            <img src={this.props.image_url} alt='ImageURL Tag' title={this.props.title}></img>
+            <p>Description: {this.props.description}</p>
             </>
         );
     }
